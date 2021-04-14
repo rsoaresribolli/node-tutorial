aa5ad80e28cf6c2228629bf9cd338c329bbd319e
Ricardo Soares Ribolli <ricardo.ribolli@inspira.com.br>
2021-04-13 22:32:45 -0300

    Set up promises

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
-const _ = require('lodash');
+const { readFile } = require('fs');
 
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
-console.log(newItems);
+const getText = (path) => {
+    return new Promise((resolve, reject) => {
+        readFile(path, 'utf8', (err, data) => {
+            if(err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    });
+};
+
+
+const start = async() => {
+    try {
+        const first = await getText('./content/first.txt');
+        const second = await getText('./content/second.txt');
+        console.log(first, second);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
+// getText('./content/first.txt')
+// .then(result => console.log(result))
+// .catch((err) => console.log(err));
